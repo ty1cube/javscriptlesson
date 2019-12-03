87d89508e9cbf5d30f03d6e649bf755732584a87
Azeez Akinsola <harkzed@gmail.com>
2019-12-03 14:10:15 +0100

    initial commit

---
+++ azeez.js
@@ -0,0 +1,68 @@
+
+// var firstName = "azeez";
+// console.log(firstName);
+// var firstName = "Azeez"
+// var age = "six"
+// var isTall = true
+// var a = true
+
+
+// String
+// Number
+// Boolean
+// undefined
+// null
+
+// Array
+// Object
+
+// function pushMe(params) {
+    
+// }
+
+// pushMe()
+
+
+
+
+
+var studentName =   ["Azeez", "tosin", "Femi", "tope", "Dimeji","Toro"] 
+
+var studentNameNew = studentName.push("Dorcas")
+
+console.log(studentName)
+//[]
+// var studentName = new Array()
+
+// console.log(studentName[-1])
+console.log(studentName.length);
+
+// var studentNameAge = ["Azeez", 5, "Tosin", true, null]
+// console.log(studentNameAge)
+
+
+// // var studentAge = new Array(7,8,8)
+
+
+// // Object
+// curly
+
+
+// var height = "5.4m"
+// var girl = 'f'    "eat"
+
+var person = {
+    firstName:"Tosin",
+    Age: 70,
+    Height: "1.8m",
+    color: [4, "cool", true]
+}
+
+
+
+console.log(person.firstName)
+console.log(person.color[2])
+
+// var person = new Object()
+
+
